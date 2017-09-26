cfc48891d4386b52075623b22b79e3c0a92338b4
Luke Hill <luke.hill@onthebeach.co.uk>
2017-09-26 16:02:53 +0100

    extra dry

--- the-language/spec/greed_dice_class.rb
+++ the-language/spec/greed_dice_class.rb
@@ -21,35 +21,20 @@ class GreedCalculator
 
   def five_score
     if count(5) >= 3
-      500 + (50 * (count(1) - 3))
+      500 + (50 * (count(5) - 3))
     else
       50 * count(5)
     end
   end
 
   def other_score
-    if count(2) >= 3
-      200
-    elsif count(3) >= 3
-      300
-    elsif count(4) >= 3
-      400
-    elsif count(6) >= 3
-      600
-    else
-      0
+    [2, 3, 4, 6].inject(0) do |sum, value|
+      sum + (100 * value) if count(value) >= 3
+      sum
     end
   end
 
   def count(value)
-    if sorted[value]
-      sorted[value].length
-    else
-      0
-    end
-  end
-
-  def sorted
-    dice.group_by { |x| x }
+    dice.count(value)
   end
 end
